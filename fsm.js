7253f8827848738ba4e2c724a9b4dae319d9b9a2
Kyle Temkin <ktemkin@binghamton.edu>
2012-11-09 18:59:21 -0500

    NOBUG initial work on toolbar

--- fsm.js
+++ fsm.js
@@ -26,6 +26,40 @@
  OTHER DEALINGS IN THE SOFTWARE.
 */
 
+/*
+ * base64.js - Base64 encoding and decoding functions
+ *
+ * See: http://developer.mozilla.org/en/docs/DOM:window.btoa
+ *      http://developer.mozilla.org/en/docs/DOM:window.atob
+ *
+ * Copyright (c) 2007, David Lindquist <david.lindquist@gmail.com>
+ * Released under the MIT license
+ */
+
+if (typeof btoa == 'undefined') {
+    function btoa(str) {
+        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        var encoded = [];
+        var c = 0;
+        while (c < str.length) {
+            var b0 = str.charCodeAt(c++);
+            var b1 = str.charCodeAt(c++);
+            var b2 = str.charCodeAt(c++);
+            var buf = (b0 << 16) + ((b1 || 0) << 8) + (b2 || 0);
+            var i0 = (buf & (63 << 18)) >> 18;
+            var i1 = (buf & (63 << 12)) >> 12;
+            var i2 = isNaN(b1) ? 64 : (buf & (63 << 6)) >> 6;
+            var i3 = isNaN(b2) ? 64 : (buf & 63);
+            encoded[encoded.length] = chars.charAt(i0);
+            encoded[encoded.length] = chars.charAt(i1);
+            encoded[encoded.length] = chars.charAt(i2);
+            encoded[encoded.length] = chars.charAt(i3);
+        }
+        return encoded.join('');
+    }
+}
+
+
 function Link(a, b) {
   this.nodeA = a;
   this.nodeB = b;
@@ -679,30 +713,42 @@ function resetCaret() {
 }
 
 var canvas;
+var nodes = [];
+var links = [];
+
+/** 
+ * Node Configuration.
+ */
 var nodeRadius = 55;
 var nodeOutline = 2;
 var nodeFgColor = "black";
 var nodeBgColor = "white";
 var nodeSelectedColor = "blue";
-var nodeFont = '16px "Arial", sans-serif'
+var nodeFont = '16px "Droid Sans", sans-serif'
 var nodeOutputPadding = 14; //pixels
-var nodeOutputFont = '20px "Courier New", monospace'
+var nodeOutputFont = '20px "Inconsolata", monospace'
 var nodeOutputColor = "#101010";
-var nodes = [];
 
-var linkFont = '16px "Courier New", monospace'
+/**
+ * Link (/Transition/Arc) Configuration
+ */
+var linkFont = '16px "Inconsolata", monospace'
 var linkFgColor = "black";
 var linkBgColor = "white";
 var linkSelectedColor = "blue";
-var links = [];
 
-var cursorVisible = true;
+/**
+ * General Config
+ */
 var snapToPadding = 20; // pixels
 var hitTargetPadding = 20; // pixels
+
+
+var originalClick;
+var cursorVisible = true;
 var selectedObject = null; // either a Link or a Node
 var currentLink = null; // a Link
 var movingObject = false;
-var originalClick;
 var inOutputMode = false; //determines if we're in edit-output mode
 
 function drawUsing(c) {
